d51f6e91c508adbf56a12722b3cd5cd903722dcb
elyas-kosari <elyas.kosari@gmail.com>
2021-10-21 12:17:27 -0400

    Add MembershipType POCO and corresponding changes needed for it

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubsctibedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; } // Navigation Property
+        public byte MembershipTypeId { get; set; } // Foreign Key --> The same type with Id in MembershipType POCO and the name (POCO + Id) i.e. MembershipTypeId
     }
 }
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; } 
+        public short SignIpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
