b3a5be5faf240eb6694b52bfaa812a91879e3516
elyas-kosari <elyas.kosari@gmail.com>
2021-10-21 12:41:43 -0400

    Apply data annotations to Customer class

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubsctibedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; } // Navigation Property
